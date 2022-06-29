4cd4e779e0fcbcdf5f9d1d8abf8fd68fbb1a0b42
Revda <revda.1905@gmail.com>
2022-06-29 23:44:45 +0300

    footer items added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import {SiNotion} from "react-icons/si";
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
+    <div className='flex mt-5 justify-center'> 
+        
+       
+        
+  <div className="flex-1 ml-5">
+    <div className="flex gap-3 ">
+     <SiNotion fontSize={30}/>
+     <div className=""> Notion</div>
+     </div>
+     </div>
+  <div className="contents">
+    <div className="flex-1 ...">Product</div>
+    <div className="flex-1 ...">Product</div>
+    <div className="flex-1 ...">Product</div>
+    <div className="flex-1 ...">Product</div>
+  </div>
+  
+</div>
+        
   )
 }
 export default Footer
